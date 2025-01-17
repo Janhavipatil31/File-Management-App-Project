c5b4485e0160ca1d7b85b9d1005585fe4440b575
Janhavipatil31 <164491102+Janhavipatil31@users.noreply.github.com>
2025-01-17 19:02:57 +0530

    Add files via upload
    •	Developed a Python-based File Management System.
    •	Leveraged OS modules for seamless file handling.
    •	Implemented efficient looping mechanisms.
    •	Designed a user-friendly desktop application.
    •	Demonstrated expertise in file handling, functions, and modular code organization.

---
+++ read.py
@@ -0,0 +1,4 @@
+file=open('Untitled-1.txt','r')
+content=file.read()
+file.close()
+print(f"content of 'Untitled-1.txt':{content}")
---
+++ write.py
@@ -0,0 +1,3 @@
+file=open('Untitled-1.txt','w')
+file.write('hello this is first file of python')
+file.close()
